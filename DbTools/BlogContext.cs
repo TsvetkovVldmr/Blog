9f502c3012342b789523644a831875c0af3fec29
Vladimir Tsvetkov <tsvetkov.vldmr@gmail.com>
2020-05-05 19:53:51 +0300

    FirstCommit

---
+++ DbTools/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Resources;
+
+namespace DbTools
+{
+    public class BlogContext : DbContext
+    {
+        //@"Data Source=DESKTOP-EJ2NR7K\SQLEXPRESS;Initial Catalog=BlogDB;Integrated Security=True"
+
+        public BlogContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-EJ2NR7K\SQLEXPRESS;Initial Catalog=BlogDB;Integrated Security=True;");
+        }
+
+    }
+}

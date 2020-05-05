af36593d191e0e897c99fc8a816fcb6d6bce65d9
Vladimir Tsvetkov <tsvetkov.vldmr@gmail.com>
2020-05-05 19:57:07 +0300

    Blog context temporary finished

--- DbTools/BlogContext.cs
+++ DbTools/BlogContext.cs
@@ -4,10 +4,11 @@ using System.Resources;
 
 namespace DbTools
 {
-    public class BlogContext : DbContext
+    /// <summary>
+    /// Контекст БД
+    /// </summary>
+    public partial class BlogContext : DbContext
     {
-        //@"Data Source=DESKTOP-EJ2NR7K\SQLEXPRESS;Initial Catalog=BlogDB;Integrated Security=True"
-
         public BlogContext()
         {
             Database.EnsureCreated();
